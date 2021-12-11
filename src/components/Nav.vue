9a7628a568a20be569d5930836573ca44cb1e50a
freeton20 <doctor4891@gmail.com>
2021-12-12 00:12:50 +0200

    fix

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
-  <b-navbar toggleable="lg" type="dark" variant="dark">
-    <b-navbar-brand href="#" style="margin-left: 5rem; color: yellow; font-weight: bold;">NODE RADAR 📡 <span style="color:gray">(fake data now)</span></b-navbar-brand>     
+  <b-navbar  style="z-index: 1000;" toggleable="lg" type="dark" variant="dark">
+    <b-navbar-brand href="#" style="margin-left: 5rem; color: yellow; font-weight: bold;">NODE RADAR 📡 <span style="color:gray">(updated 11.12.2021)</span></b-navbar-brand>     
   </b-navbar>
 </div>
 </template>
